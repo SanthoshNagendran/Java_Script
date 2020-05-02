1d1a7650d449d7986f4933024410f42b007a27df
santhosh <santhoshguptha95@gmail.com>
2020-05-02 19:33:54 -0400

    My project

---
+++ Java_Practise.js
@@ -0,0 +1,14 @@
+var Sum = {
+    A: 1,
+    B: 2,
+    C: 3,
+    F: 5;
+};
+
+var Differ = Sum;
+
+var arrayOfStuff = [
+    3, 'a', { Name: 'Santhosh', Age: 22 }, [2, 3, 4], true, 'Infinity'
+];
+
+
